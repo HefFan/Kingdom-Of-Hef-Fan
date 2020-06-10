122c821bc2dcab52af472a599ef1498608fc2128
Hef Fan Kingdom <heffankingdom@gmail.com>
2020-06-10 16:11:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.getElementById("rghtdir1").style.visibility= "hidden";
+document.getElementById("pub_chat").style.visibility= "visible";
+document.getElementById("leftdir2").style.visibility= "hidden";
+document.getElementById("cont_box").style.visibility= "visible";
+
+
 
 
 function submit(){
@@ -13,4 +19,38 @@ function submit(){
 	document.getElementById("after-sign").innerHTML= a;
 	document.getElementById("after-sign-form").innerHTML= nol; 
 	}
-console.log("kokoh");
+
+function hidePubChat() {
+
+	document.getElementById("pub_chat").style.visibility= "hidden";
+	document.getElementById("rghtdir1").style.visibility= "visible";
+	document.getElementById("leftdir1").style.visibility= "hidden";
+	}
+
+
+
+
+function showPubChat() {
+
+	document.getElementById("pub_chat").style.visibility= "visible";
+	document.getElementById("rghtdir1").style.visibility= "hidden";
+	document.getElementById("leftdir1").style.visibility= "visible";
+	}
+
+
+function hideContBox() {
+
+	document.getElementById("cont_box").style.visibility= "hidden";
+	document.getElementById("leftdir2").style.visibility= "visible";
+	document.getElementById("rghtdir2").style.visibility= "hidden";
+	}
+
+function showContBox() {
+
+	document.getElementById("cont_box").style.visibility= "visible";
+	document.getElementById("leftdir2").style.visibility= "hidden";
+	document.getElementById("rghtdir2").style.visibility= "visible";
+	}
+
+
+
